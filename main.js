6fab9508422a41ba843f56ac26b6676d01a93d34
Saurabh Kadam <sourabhkadam660@gmail.com>
2020-12-30 19:54:57 +0530

    myfirst commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector('.menu-btn');
+const navList = document.querySelectorAll('.hidden-li');
+const nav = document.querySelector('.navbar');
+
+btn.addEventListener('click', () =>{
+    nav.classList.toggle('h-resp');
+});
+
+let menu = false;
+
+btn.addEventListener('click', toggle);
+
+function toggle() {
+    if(!menu){
+        navList.forEach(item => item.classList.add('show'));
+
+        menu = true;
+    }else{
+        navList.forEach(item => item.classList.remove('show'));
+
+        menu = false;
+    }
+}
